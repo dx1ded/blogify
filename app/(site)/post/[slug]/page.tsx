ce630035a5ee7c45d1067742977b7ff5236a619f
dx1ded <doskovol1@gmail.com>
2024-11-17 23:06:14 -0600

    Edited Dockerfile, env, schema.prisma and generated a new build

--- app/(site)/post/[slug]/page.tsx
+++ app/(site)/post/[slug]/page.tsx
@@ -7,30 +7,6 @@ import prisma from "~/prisma"
 import { CommentList, CommentListSkeleton } from "~/shared/ui/CommentList"
 import { Post } from "~/shared/ui/Post"
 
-export async function generateStaticParams() {
-  const posts = await prisma.post.findMany({
-    include: {
-      author: {
-        select: {
-          id: true,
-          name: true,
-          image: true,
-        },
-      },
-      _count: {
-        select: { likedBy: true },
-      },
-      likedBy: {
-        select: {
-          id: true,
-        },
-      },
-    },
-  })
-
-  return posts.map((post) => post.slug)
-}
-
 export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
   const post = await prisma.post.findFirst({
     where: { slug: params?.slug },
@@ -43,9 +19,6 @@ export async function generateMetadata({ params }: { params: { slug: string } })
 
   return { title: post.title }
 }
-
-// Revalidate every hour
-export const revalidate = 3600
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const session = await getServerSession(authOptions)
